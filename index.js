2d7e5a354d3a0a35d4dbb444333e3f62529dc3eb
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-10-03 20:58:24 +0530

    learn loops

--- index.js
+++ index.js
@@ -57,3 +57,21 @@ switch(num){
     break;
     default: console.log('E');
 }
+
+for(let i =0; i< 5; i=i+1){
+        console.log(i);
+    }
+    
+    let i=0;
+    while(i < 10)
+    {
+        console.log(i);
+        i++;
+    }
+    
+    
+    let i=0;
+    do{
+        console.log(i);
+        i++;
+    }while(i<9);
