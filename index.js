981c8b86b5b365f89bb0b5ea5ce620178454c55a
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-10-03 20:54:43 +0530

    learn ifelse

--- index.js
+++ index.js
@@ -23,3 +23,29 @@ let voteStatus = (age >= 18) ? 'Eligible For Vote' : 'Not Eligible For Vote';
 
 console.log(voteStatus)
 
+
+console.log('Ram Ram Bhai Saareyaane Pranaam');
+
+
+
+
+// let marks = 75;
+
+// if (marks >= 90){
+//     console.log('A');
+// }
+
+// else if (marks >= 80){
+//     console.log('B')
+// }
+
+// else if (marks >= 70){
+//     console.log('C')
+// }
+// else if (marks >= 60){
+//     console.log('D')
+// }
+// else{
+//     console.log('E')
+// }
+
