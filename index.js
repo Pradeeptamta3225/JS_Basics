fe8a605b5958c2de5ed95a31357d72605c2dc067
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-10-03 20:53:01 +0530

    work on console

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
     console.log('Ram Ram Bhai Saareyaane Pranaam');
 
 
+let a = 6;
+
+console.log(a);
+
+
+let lastName = ("Tamta");
+console.log(lastName);
+
+lastName = 3;
+console.log(lastName);
+
+
+console.log(3<4);
+
+
+let age = 19;
+
+let voteStatus = (age >= 18) ? 'Eligible For Vote' : 'Not Eligible For Vote';
+
+console.log(voteStatus)
+
