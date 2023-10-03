3d6337cbedbc23b47fba302180d9c8a9b3bec2a4
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-10-03 20:57:28 +0530

    learn switch case

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-    console.log('Ram Ram Bhai Saareyaane Pranaam');
+console.log('Ram Ram Bhai Saareyaane Pranaam');
 
 
 let a = 6;
@@ -24,28 +24,36 @@ let voteStatus = (age >= 18) ? 'Eligible For Vote' : 'Not Eligible For Vote';
 console.log(voteStatus)
 
 
-console.log('Ram Ram Bhai Saareyaane Pranaam');
+let marks = 75;
 
+if (marks >= 90){
+    console.log('A');
+}
 
+else if (marks >= 80){
+    console.log('B')
+}
 
+else if (marks >= 70){
+    console.log('C')
+}
+else if (marks >= 60){
+    console.log('D')
+}
+else{
+    console.log('E')
+}
 
-// let marks = 75;
+let num = 4;
 
-// if (marks >= 90){
-//     console.log('A');
-// }
-
-// else if (marks >= 80){
-//     console.log('B')
-// }
-
-// else if (marks >= 70){
-//     console.log('C')
-// }
-// else if (marks >= 60){
-//     console.log('D')
-// }
-// else{
-//     console.log('E')
-// }
-
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('E');
+}
