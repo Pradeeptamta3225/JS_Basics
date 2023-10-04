5b090344e995950bbe29d725b2525509c0184bed
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-10-04 20:04:31 +0530

    learn object creation

--- index.js
+++ index.js
@@ -2,76 +2,90 @@
 console.log('Ram Ram Bhai Saareyaane Pranaam');
 
 
-let a = 6;
+// let a = 6;
 
-console.log(a);
-
-
-let lastName = ("Tamta");
-console.log(lastName);
-
-lastName = 3;
-console.log(lastName);
+// console.log(a);
 
 
-console.log(3<4);
+// let lastName = ("Tamta");
+// console.log(lastName);
+
+// lastName = 3;
+// console.log(lastName);
 
 
-let age = 19;
-
-let voteStatus = (age >= 18) ? 'Eligible For Vote' : 'Not Eligible For Vote';
-
-console.log(voteStatus)
+// console.log(3<4);
 
 
-let marks = 75;
+// let age = 19;
 
-if (marks >= 90){
-    console.log('A');
-}
+// let voteStatus = (age >= 18) ? 'Eligible For Vote' : 'Not Eligible For Vote';
 
-else if (marks >= 80){
-    console.log('B')
-}
+// console.log(voteStatus)
 
-else if (marks >= 70){
-    console.log('C')
-}
-else if (marks >= 60){
-    console.log('D')
-}
-else{
-    console.log('E')
-}
 
-let num = 4;
+// let marks = 75;
 
-switch(num){
-    case 1: console.log('A');
-    break;
-    case 2: console.log('B');
-    break;
-    case 3: console.log('C');
-    break;
-    case 4: console.log('D');
-    break;
-    default: console.log('E');
-}
+// if (marks >= 90){
+//     console.log('A');
+// }
 
-for(let i =0; i< 5; i=i+1){
-        console.log(i);
+// else if (marks >= 80){
+//     console.log('B')
+// }
+
+// else if (marks >= 70){
+//     console.log('C')
+// }
+// else if (marks >= 60){
+//     console.log('D')
+// }
+// else{
+//     console.log('E')
+// }
+
+
+// let num = 4;
+
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     case 4: console.log('D');
+//     break;
+//     default: console.log('E');
+// }
+
+// for(let i =0; i< 5; i=i+1){
+//     console.log(i);
+// }
+
+// let i=0;
+// while(i < 10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<9);
+
+object create 
+let rectangle = {
+    length: 1,
+    width: 2,
+
+
+    draw: function(){
+        console.log("Hello Baccho")
     }
-    
-    let i=0;
-    while(i < 10)
-    {
-        console.log(i);
-        i++;
-    }
-    
-    
-    let i=0;
-    do{
-        console.log(i);
-        i++;
-    }while(i<9);
+};
+
+
