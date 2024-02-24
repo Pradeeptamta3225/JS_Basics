f43f5ea0d261f03298025fce684bb73c707b6bb4
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-02-24 15:54:20 +0530

    practice basic js variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId = 14513;
+let accountEmail = "Pradeep@gmail.com";
+var accountPassword = "9385";
+accountCity = "Tanakpur";
+
+
+// accountId = 74839;
+accountEmail = "tamta@gmail.com";
+accountPassword= "12345";
+accountCity = "Champawat";
+
+console.log(accountId);
+
+console.table([ accountId, accountEmail, accountPassword, accountCity]);
+
