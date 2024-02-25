dc2464ced8d46e9ba9e5b862b2169c17aac79138
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-02-25 19:27:53 +0530

    practice conversionOperations

---
+++ conversionOperation.js
@@ -0,0 +1,19 @@
+let age = NaN
+
+let valueInNumber = Number(age);
+
+// console.log(valueInNumber)
+// console.log(typeof (age))
+
+
+// let result = 1
+
+// let resultInBoolean = Boolean(result)
+// console.log(resultInBoolean)
+
+
+let naam = 34
+let nameInString = String(naam)
+console.log(nameInString)
+console.log(typeof nameInString)
+// console.log(typeof naam)
