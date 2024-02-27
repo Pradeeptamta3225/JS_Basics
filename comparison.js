078d24a1f7b8b68eb41fb4bd68d6a845d0845136
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-02-27 12:31:39 +0530

    practice comparison of datatypes

---
+++ comparison.js
@@ -0,0 +1,19 @@
+console.log(null > 0)
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null <= 0)
+
+
+console.log(undefined  > 0)
+console.log(undefined  < 0)
+console.log(undefined  == 0)
+console.log(undefined  >= 0)
+console.log(undefined  <= 0)
+
+
+// Strict Check  ===
+
+console.log("2" == 2)     */ Check only Datavalue */
+
+console.log("2" === 2)   */ Check both Datavalue and DataTypes */
