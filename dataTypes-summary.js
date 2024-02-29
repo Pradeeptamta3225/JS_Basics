d91c29fcf1a2678e1e85a07bdb32ddb265a19092
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-02-29 16:07:56 +0530

    learn stack and heap memory

--- dataTypes-summary.js
+++ dataTypes-summary.js
@@ -12,7 +12,7 @@ let bigNumber = 9853894723797952395723967289n   //BigInt
 let id = Symbol('1234')
 let anotherId = Symbol('1234')
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 let todayTemp = null   //null != 0
 
@@ -42,4 +42,32 @@ let myFunction = function(){
     console.log("Hello Pradeep")
 }
 
-console.log(typeof student)
+// console.log(typeof student)
+
+
+
+
+// Stack (Primitive)  and Heap(Non-Primitive)
+
+let myYouTube = "Pradeeptamta"
+let anotherYoutube = myYouTube
+
+anotherYoutube = "abc@gmail"
+
+
+console.log(myYouTube)
+console.log(anotherYoutube)
+
+
+let userOne = {
+    email: "pradeeptamta1650@gmail.com",
+    address: "xyz"
+}
+
+let userTwo = userOne
+
+userTwo.email = "xyz@gmail.com"
+
+
+console.log(userOne.email)
+console.log(userTwo.email)
