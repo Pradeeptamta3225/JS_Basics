981f8de6cb553cc73878ed9dbdac64626786d7a0
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-04 11:52:42 +0530

    practice number

---
+++ numberandmaths.js
@@ -0,0 +1,17 @@
+let score = 500
+console.log(score)
+
+let balance = new Number(100)
+console.log(balance)
+
+
+console.log(balance.toLocaleString())
+
+
+let otherNumber = 100.435
+console.log(otherNumber.toFixed(2))
+
+console.log(otherNumber.toPrecision(4))
+
+let anotherNumber = 100000000
+console.log(anotherNumber.toLocaleString("en-IN"))
