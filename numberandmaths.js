48b17aba36ed5abb71b715bb1aabb2e47519099e
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-04 14:49:49 +0530

    math practice

--- numberandmaths.js
+++ numberandmaths.js
@@ -1,17 +1,38 @@
 let score = 500
-console.log(score)
+// console.log(score)
 
 let balance = new Number(100)
-console.log(balance)
+// console.log(balance)
 
 
-console.log(balance.toLocaleString())
+// console.log(balance.toLocaleString())
 
 
 let otherNumber = 100.435
-console.log(otherNumber.toFixed(2))
+// console.log(otherNumber.toFixed(2))
 
-console.log(otherNumber.toPrecision(4))
+// console.log(otherNumber.toPrecision(4))
 
 let anotherNumber = 100000000
-console.log(anotherNumber.toLocaleString("en-IN"))
+// console.log(anotherNumber.toLocaleString("en-IN"))
+
+
+// *********************Math*************************8
+
+
+console.log(Math)
+
+console.log(Math.abs(-5))
+console.log(Math.ceil(5.6))
+console.log(Math.round(5.4))
+console.log(Math.max(4,3,5,6,8))
+console.log(Math.min(4,3,5,6,8))
+console.log(Math.random() *10 + 1)
+console.log(Math.floor(Math.random() * 10) +1)
+
+
+
+let min = 1000
+let max = 9999
+
+console.log(Math.floor(Math.random() * (max- min + 1)) +min)
