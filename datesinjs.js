63a38d65a2582c5bb28b10fe9a46b3f35a437c66
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-05 13:35:43 +0530

    date practice

--- datesinjs.js
+++ datesinjs.js
@@ -1,16 +1,30 @@
 
 
-let newDate = new Date
+// let newDate = new Date
 
-console.log(newDate)
-console.log(newDate.toLocaleString())
-console.log(newDate.toDateString())
-console.log(newDate.toISOString())
-console.log(newDate.toJSON())
-console.log(newDate.toLocaleDateString())
-console.log(newDate.toLocaleTimeString())
-console.log(newDate.toString())
-
-
+// console.log(newDate)
+// console.log(newDate.toLocaleString())
+// console.log(newDate.toDateString())
+// console.log(newDate.toISOString())
+// console.log(newDate.toJSON())
+// console.log(newDate.toLocaleDateString())
+// console.log(newDate.toLocaleTimeString())
+// console.log(newDate.toString())
 
 
+
+
+// let newDate = Date.now()
+// console.log(Math.floor(newDate /1000))
+
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay())
+
+
+let yearDate = newDate.toLocaleString('default', {
+                 year: "numeric"})
+
+
+console.log(yearDate)
