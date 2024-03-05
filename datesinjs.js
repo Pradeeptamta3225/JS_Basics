40541409036d7496971ad668000bf8680f7b6a9f
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-05 13:23:35 +0530

    practice date

---
+++ datesinjs.js
@@ -0,0 +1,16 @@
+
+
+let newDate = new Date
+
+console.log(newDate)
+console.log(newDate.toLocaleString())
+console.log(newDate.toDateString())
+console.log(newDate.toISOString())
+console.log(newDate.toJSON())
+console.log(newDate.toLocaleDateString())
+console.log(newDate.toLocaleTimeString())
+console.log(newDate.toString())
+
+
+
+
