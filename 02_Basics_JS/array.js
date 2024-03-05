a60688446a0cdbfd0546ba1e9eda309e19c33ba6
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-05 20:45:08 +0530

    practice array

--- 02_Basics_JS/array.js
+++ 02_Basics_JS/array.js
@@ -9,10 +9,21 @@
 
 // ***********Array Method************
 
-let myArr = [1, 2, 3, 4, 5]
-myArr.push(6)
-myArr.push(9)
-myArr.pop()
-myArr.unshift(9)
-myArr.shift()
-console.log(myArr)
+let myArr = [1, 2, 3, 4, 5, 6]
+// myArr.push(6)
+// myArr.push(9)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr)
+
+console.log("A ", myArr)
+
+let myArr1 = myArr.slice(1, 3)
+
+console.log(myArr1)
+console.log("B ", myArr)
+
+let myArr2 = myArr.splice(1, 3)
+console.log("C ", myArr)
+console.log(myArr2)
