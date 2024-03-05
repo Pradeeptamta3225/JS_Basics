d3c98c3d40df9419da9ac0f51d4b9f0ddf3f9710
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-05 20:24:26 +0530

    practice array

---
+++ 02_Basics_JS/array.js
@@ -0,0 +1,18 @@
+
+
+// **************** Array ****************
+
+// let number = [1, 2, 3, 4, 5]
+// console.log(number)
+// console.log(number[2])
+
+
+// ***********Array Method************
+
+let myArr = [1, 2, 3, 4, 5]
+myArr.push(6)
+myArr.push(9)
+myArr.pop()
+myArr.unshift(9)
+myArr.shift()
+console.log(myArr)
