86bd3279eb9e8f5d6c7027a994b6a7d1ab7e15b2
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-06 15:11:02 +0530

    practice array

--- 02_Basics_JS/array.js
+++ 02_Basics_JS/array.js
@@ -17,13 +17,23 @@ let myArr = [1, 2, 3, 4, 5, 6]
 // myArr.shift()
 // console.log(myArr)
 
-console.log("A ", myArr)
+// console.log("A ", myArr)
 
-let myArr1 = myArr.slice(1, 3)
+// let myArr1 = myArr.slice(1, 3)
 
-console.log(myArr1)
-console.log("B ", myArr)
+// console.log(myArr1)
+// console.log("B ", myArr)
 
-let myArr2 = myArr.splice(1, 3)
-console.log("C ", myArr)
-console.log(myArr2)
+// let myArr2 = myArr.splice(1, 3)
+// console.log("C ", myArr)
+// console.log(myArr2)
+
+
+
+let indianHeros = ["shaktiman", "arymaan", "Gunior G"]
+let marvalHeros = ["Ironman", "thor", "Thanos"]
+
+indianHeros.push(marvalHeros)
+console.log(indianHeros.length)
+
+
