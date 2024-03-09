89c1844cd553be6a0857d6d6f96ba0b91e032b38
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-09 16:23:16 +0530

    object practice

--- 02_Basics_JS/objects.js
+++ 02_Basics_JS/objects.js
@@ -11,3 +11,20 @@ let myobj = {
 
 console.log(typeof myobj[mySym]);
 console.log(myobj["age"]);
+myobj.email = "tamta1650@gmail.com"
+// console.log(myobj);
+// Object.freeze(myobj)
+myobj.name = "tamta"
+// console.log(myobj);
+
+myobj.gift = function(){
+    console.log("Kya Haal Hai");
+}
+
+myobj.giftTwo = function(){
+    console.log(`Kya Haal Hai,  ${this.name}`);
+}
+
+
+console.log(myobj.gift());
+console.log(myobj.giftTwo());
