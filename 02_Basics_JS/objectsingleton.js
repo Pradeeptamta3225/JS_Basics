5e245656a02b2fc0a5e77ff73ab91bd8bf5faabc
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-13 15:22:00 +0530

    practice object

--- 02_Basics_JS/objectsingleton.js
+++ 02_Basics_JS/objectsingleton.js
@@ -30,9 +30,27 @@ let obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
 
-console.log(happenUser);
-console.log(Object.values(happenUser));
-console.log(Object.keys(happenUser));
-console.log(Object.entries(happenUser));
+// console.log(happenUser);
+// console.log(Object.values(happenUser));
+// console.log(Object.keys(happenUser));
+// console.log(Object.entries(happenUser));
 
-console.log(happenUser.hasOwnProperty("name"));
+// console.log(happenUser.hasOwnProperty("name"));
+
+
+let course = {
+    name: "100 Days of Job",
+    price: 999,
+    courseTeacher: "pradeep"
+}
+
+let {courseTeacher} = course          // Destructuring
+let {courseTeacher: guru} = course  
+console.log(guru);
+
+
+[
+    {},            //API Formate
+    {},
+    {}
+]
