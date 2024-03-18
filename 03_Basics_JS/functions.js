6ba664e5ae286296401a84932fabca06b9fce0cc
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-18 10:03:59 +0530

    practice functins

---
+++ 03_Basics_JS/functions.js
@@ -0,0 +1,21 @@
+
+
+function myName(){
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("D");
+    console.log("E");
+    console.log("E");
+    console.log("P");
+
+}
+
+// myName()
+
+function plus(num1, num2){
+    console.log(num1 + num2);
+}
+
+// plus(2," pradeep")
+plus(2,7)
