ba943d73555e72ecae797f13c33ae56feebc9c0f
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-19 12:23:22 +0530

    practice scope

---
+++ 03_Basics_JS/scope.js
@@ -0,0 +1,18 @@
+
+// var c = 500
+
+let a = 50;
+
+if (true) {
+    
+let a = 10;
+const b = 20;
+var c = 30;
+
+console.log("Scope:", a);
+}
+
+// console.log(a);
+// console.log(b);
+console.log(a);
+
