6e3e1492f2c97e3393c8b3dbaee363664b51e368
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-19 22:15:13 +0530

    practice scope

--- 03_Basics_JS/scope.js
+++ 03_Basics_JS/scope.js
@@ -9,10 +9,52 @@ let a = 10;
 const b = 20;
 var c = 30;
 
-console.log("Scope:", a);
+// console.log("Scope:", a);
 }
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
+
+function one(){
+    const username = 'Pradeep'
+
+    function two(){
+        const account = ' Facebook';
+        console.log(username + account)
+    }
+
+    two()
+}
+
+one()
+
+
+
+if (true) {
+    const username = 'pradeep'
+
+    if (username === 'pradeep') {
+        const website = ' instagram'
+        console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+//Hoisting Overview
+console.log(addone(3));
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5));              // Hoisting is not allow in stored variable
+const addTwo = function(num) {
+    return num + 2
+}
 
