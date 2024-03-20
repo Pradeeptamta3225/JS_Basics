60a44e132b324f35d3d29dd2d4d49c730db2e4b1
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-20 17:58:38 +0530

    practice this method and arrow function

--- 03_Basics_JS/arrow.js
+++ 03_Basics_JS/arrow.js
@@ -5,8 +5,8 @@ const user = {
     price: 199,
 
     welcomeMessage: function(){
-                console.log(` ${this.username} , welcome to the code `);
-                console.log(this);
+                // console.log(` ${this.username} , welcome to the code `);
+                // console.log(this);
     }
 }
 
@@ -15,3 +15,57 @@ user.username = "sam"
 user.welcomeMessage()
 
 // console.log(this);
+
+function one() {
+    let username = 'pradeep'
+    // console.log(this.username);
+}
+
+one()
+
+
+const two = function (){
+    let username = 'pradeep'
+    // console.log(this.username);
+}
+
+two()
+
+
+
+
+const Two = () => {
+    let username = 'pradeep'
+    // console.log(this.username);
+}
+
+Two()
+
+
+
+//+++++++++++++++++  Function   ++++++++++++++++
+
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwo(3,5));
+
+
+const addthree = (num1, num2, num3) => num1 + num2 + num3                //implicit
+
+console.log(addthree(2,5,7));
+
+
+
+const naam = () => ({username: 'Pradeep'})
+
+console.log(naam());
+
+
+
+
+
+
+
