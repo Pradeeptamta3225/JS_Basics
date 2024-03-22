e0df04c8e551b0c3bc7f860a4b4fdae5ef06dc64
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-22 15:02:42 +0530

    practice control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,41 @@
+
+
+// let month = 4
+
+// switch (month) {
+//     case 1:
+//         console.log('jan');
+//         break;
+//     case 2:
+//         console.log('feb');
+//         break;
+//     case 3:
+//         console.log('mar');
+//         break;
+//     case 4:
+//         console.log('apr');
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 'march'
+
+switch (month) {
+    case "jan":
+        console.log('jan');
+        break;
+    case 'feb':
+        console.log('feb');
+        break;
+    case 'march':
+        console.log('mar');
+        break;
+    case 'apr':
+        console.log('apr');
+        break;
+
+    default:
+        break;
+}
