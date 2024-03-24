926da26ce67f085d051f4ee017d715f830c58dda
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-24 20:16:12 +0530

    practice loop

---
+++ 05_iteration/loops.js
@@ -0,0 +1,31 @@
+
+
+for (let i = 1; i <=5; i++) {
+    const element = i;
+    // console.log(element);
+    
+}
+
+
+for (let i = 0; i <=5; i++) {
+    const element = i;
+    for (let j = 0; j <=5; j++) {
+        const element = j;
+        // console.log(`Inner loop J =  ${element}]`);
+    }
+    // console.log(`Outer Loop I = ${element}`);
+    
+}
+
+
+for (let i = 4; i <= 4 ; i++) {
+    const element = i;
+    console.log(`Outer Loop : ${i}`);
+
+    for (let J = 1; J <= 10; J++) {
+        const element = J;
+        console.log(i + '*' + J + ' = ' + i*J );
+    }
+    
+
+}
