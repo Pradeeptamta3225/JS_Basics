a5dd3afc1a26f16825bb1bd9d6fa52c235f6491c
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-27 22:17:01 +0530

    practice control flow

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -13,3 +13,16 @@ while (arr < myArray.length) {
     console.log(`value of array is ${myArray[arr]}`);
     arr = arr + 1
 }
+
+
+let score = 1
+do {
+    console.log(`Value is ${score}`);
+    score++
+} while (score < 10);
+
+let core = 12
+do {
+   console.log(`Value is ${core}`);
+   core++
+} while (core < 10);
