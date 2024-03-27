0a62a92cb86283f8f061ac00330fbfd1d9d96291
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-03-27 22:09:26 +0530

    practice control flow

---
+++ 05_iteration/two.js
@@ -0,0 +1,15 @@
+
+let index = 2
+while (index <= 20) {
+    
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["shaktiman", "aryaman", "ironman"]
+arr = 0
+while (arr < myArray.length) {
+    console.log(`value of array is ${myArray[arr]}`);
+    arr = arr + 1
+}
