3763f8bab96b37f08d715acb47387144670365fb
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-03 21:08:50 +0530

    practice foreach loop

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -3,8 +3,18 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, ,9, 10];
 
-const newNums = myNums.filter( (num) => {
-   return num > 5
+// const newNums = myNums.filter( (num) => {
+//    return num > 5
+// })
+
+const newNums = []
+
+myNums.forEach( (num) => {
+   if (num > 5) {
+      newNums.push(num)
+   }
 })
 
 console.log(newNums);
+
+
