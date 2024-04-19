958bec3614a2c66da6bde550e8c24c2f0201f6b4
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-19 15:33:19 +0530

    practice promises

--- 08_advance/promise.js
+++ 08_advance/promise.js
@@ -19,3 +19,39 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log("promise two is resolve");
 })
+
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Promise three");
+        resolve({username: "Pradeep", email: "pradeep@gmail.com"});
+    },1000)
+})
+
+promiseThree.then(function(user){
+        console.log(user);
+})
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username: "Pradeep", email: "pradeep@gmail.com"});
+        } else{
+                reject("something went wrong");
+        }
+    },1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username) =>{
+    console.log(username);
+}).catch((error) =>{
+    console.log(error);
+})
+.finally(()=>{
+    console.log("promise either resoved or reject");
+})
