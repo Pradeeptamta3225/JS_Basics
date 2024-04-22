f603cad74e4a7d64871929f80a59781b73267c1e
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-22 22:54:28 +0530

    practice oops concept

--- 09_OOP/oops.js
+++ 09_OOP/oops.js
@@ -1,6 +1,6 @@
 
 
-const user = {
+const userData = {
     username: "Pradeep",
     email: "xyz@gmail.com",
     age: 20,
@@ -10,5 +10,24 @@ const user = {
     }
 }
 
-console.log(this);
-console.log(user.username);
+// console.log(this);
+// console.log(userData.username);
+
+
+
+function User(username, email,age){
+    this.username = username;
+    this.email = email;
+    this.age = age;
+
+    const greeting = function(){
+        console.log(`welcome : ${this.username}`);
+    }
+
+}
+
+const userOne = new User("Pradeep", "xyz@gmail.com",23);
+const userTwo = new User("Tamta", "abc@gmail.com", 20);
+
+console.log(userOne);
+console.log(userTwo);
