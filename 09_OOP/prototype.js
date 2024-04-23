b7dccde8f0c91611d14320f1566c68e02264fc36
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-23 16:46:33 +0530

    practice prototype

--- 09_OOP/prototype.js
+++ 09_OOP/prototype.js
@@ -23,3 +23,32 @@ Array.prototype.heyPradeep = function(){
 
 myHero.heyPradeep();
 // heroPower.heyPradeep();
+
+myHero.pradeep();
+
+
+//inheritance 
+
+
+const user = {
+    name: " ",
+    login: true
+}
+
+const teacher = {
+    fulltime: true
+
+}
+
+const teacingAssistance = {
+        course: "JS",
+        assignment: true,
+        __proto__: teacher
+}
+
+
+teacher.__proto__ = user
+console.log(user);
+// modern syntax
+
+Object.setPrototypeOf(teacher, user)
