958633c5076dd9b3e4a941fbfb167559482f22ac
pradeeptamta3225 <pradeeptamta1650@gmail.com>
2024-04-27 10:25:49 +0530

    practice setter getter

---
+++ 09_OOP/getter_setter.js
@@ -0,0 +1,31 @@
+
+class User{
+    constructor(email, password){
+        this.email = email,
+        this.password = password
+    }
+
+
+    get email(){
+        return this._email
+    }
+
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return this._password
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+
+const pradeep = new User("xyz.com", "just")
+
+console.log(pradeep.password);
+
+console.log(pradeep.email);
